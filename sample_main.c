b08d4a12a5b3bae49f68dbfb4f88d932f7babd7c
xDysta <xav4098@gmail.com>
2018-12-08 16:27:31 +0100

    remise du fichier par défaut pour passer le make test

--- sample_main.c
+++ sample_main.c
@@ -14,9 +14,9 @@
 int main(int argc, char *argv[])
 {
     struct testfw_t *fw = testfw_init(argv[0], TIMEOUT, LOGFILE, COMMAND, SILENT, VERBOSE);
-    testfw_register_func(fw, "test", "failure", test_failure);
-    testfw_register_symb(fw, "othertest", "success");
-    testfw_register_suite(fw, "test");
+    testfw_register_func(fw, "test", "success", test_success);
+    testfw_register_symb(fw, "test", "failure");
+    testfw_register_suite(fw, "othertest");
     testfw_run_all(fw, argc - 1, argv + 1, TESTFW_FORKS);
     testfw_free(fw);
     return EXIT_SUCCESS;
